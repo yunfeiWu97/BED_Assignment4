2082c9ee455546393f226483e3e5a1bec1bef44f
yunfeiWu97 <wuyunfei97@gmail.com>
2025-10-15 17:48:15 -0500

    re-enable admin-only.

--- src/api/v1/routes/adminRoutes.ts
+++ src/api/v1/routes/adminRoutes.ts
@@ -1,13 +1,15 @@
 import express, { Router } from "express";
 import authenticate from "../middleware/authenticate";
-// import isAuthorized from "../middleware/authorize";
+import isAuthorized from "../middleware/authorize";
 import { setCustomClaims } from "../controllers/adminController";
 
 const router: Router = express.Router();
 
-/**
- * TEMPORARILY auth-only for bootstrap
- */
-router.post("/setCustomClaims", authenticate, setCustomClaims);
+router.post(
+  "/setCustomClaims",
+  authenticate,
+  isAuthorized({ hasRole: ["admin"] }),
+  setCustomClaims
+);
 
 export default router;
