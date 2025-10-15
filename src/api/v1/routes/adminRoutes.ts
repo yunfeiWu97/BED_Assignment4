701c9f6bc5df6ea950ab8ace759c67531b86101a
yunfeiWu97 <wuyunfei97@gmail.com>
2025-10-15 17:42:08 -0500

    temporarily setCustinClaims to auth-only for initial role seeding.

--- src/api/v1/routes/adminRoutes.ts
+++ src/api/v1/routes/adminRoutes.ts
@@ -1,15 +1,13 @@
 import express, { Router } from "express";
 import authenticate from "../middleware/authenticate";
-import isAuthorized from "../middleware/authorize";
+// import isAuthorized from "../middleware/authorize";
 import { setCustomClaims } from "../controllers/adminController";
 
 const router: Router = express.Router();
 
-router.post(
-  "/setCustomClaims",
-  authenticate,
-  isAuthorized({ hasRole: ["admin"] }),
-  setCustomClaims
-);
+/**
+ * TEMPORARILY auth-only for bootstrap
+ */
+router.post("/setCustomClaims", authenticate, setCustomClaims);
 
 export default router;
